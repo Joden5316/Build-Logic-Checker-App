822c8d0bfa1fb2047c04662fe97a49f872b88223
Joden5316 <138895167+Joden5316@users.noreply.github.com>
2026-04-15 20:07:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ let truthyOrFalsy = true;
 
 // 2. Check truthiness
 console.log(Boolean(truthyOrFalsy));
+
+// 3. Change to non-empty string
+truthyOrFalsy = "freeCodeCamp";
+console.log(Boolean(truthyOrFalsy));
