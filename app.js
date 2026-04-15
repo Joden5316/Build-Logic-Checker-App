d140ff4c8fc7f41088611a8bdf97c4e12a665256
Joden5316 <138895167+Joden5316@users.noreply.github.com>
2026-04-15 20:14:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ if (hasDeveloperJob) {
   console.log("Timmy is employed as a developer.");
 }
 
-// 8. Another variable
+// 7 Another variable
 let isTimmyAGamer = false;
 
-// 9. If statement (won’t run)
+// 8. If statement
 if (isTimmyAGamer) {
   console.log("Timmy loves to play World of Warcraft.");
+}
+
+// Age condition
+let timmyAge = 18;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
 }
